24c30c3a4800eeebbe59cf984620ac7809fc0f9f
Lusine Gevorgyan <lusinegevorgyan@Lusines-MacBook-Air-2.local>
2022-11-09 16:09:45 +0400

    Reverse an array

---
+++ ex4.js
@@ -0,0 +1,12 @@
+const arr = [3, 6, 9];
+result = [];
+for (let index = arr.length - 1; index >= 0; index--) {
+    result.push(arr[index]);
+    
+}
+// for (let index = arr.length - 1; index >= 0; index--) {
+//     result += arr[index];
+// }
+// console.log([result])
+// Used Google and found out about push so decided to use it. :)
+console.log(result)
